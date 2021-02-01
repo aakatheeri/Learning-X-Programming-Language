dfcc091e2306669ed025bf641cbff29391aa48d8
Ahmed Alkatheeri <3767316+aakatheeri@users.noreply.github.com>
2021-02-01 15:20:36 +0400

    Add Ionic app 'simple-modal-app' example

---
+++ Front-End/Ionic/simple-modal-app/src/views/Home.vue
@@ -0,0 +1,78 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Home</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content class="ion-padding">
+          <ion-modal :isOpen="modalInfo"
+                     @onDidDismiss="showModal(false)">
+               <simple-modal></simple-modal>
+          </ion-modal>
+
+         <ion-button @click="showModal(true)">
+              Show Modal
+         </ion-button>
+
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonModal, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/vue';
+import { defineComponent, ref } from 'vue';
+import SimpleModal from "./SimpleModal.vue"
+
+export default defineComponent({
+     name: 'Home',
+     components: {
+          IonModal,
+          IonContent,
+          IonHeader,
+          IonPage,
+          IonTitle,
+          IonToolbar,
+          IonButton,
+          SimpleModal
+     },
+     setup() {
+          const modalInfo = ref(false);
+          const showModal = (state: boolean) => {
+               modalInfo.value = state;
+          };
+          return { modalInfo, showModal }
+     }
+});
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+
+  color: #8c8c8c;
+
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+</style>
---
+++ Front-End/Ionic/simple-modal-app/src/views/SimpleModal.vue
@@ -0,0 +1,65 @@
+<template>
+
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>{{ title }}</ion-title>
+        <ion-buttons slot="end">
+             <ion-button @click="dismissModal()">Close</ion-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content class="ion-padding">
+         {{ content }}
+    </ion-content>
+  </ion-page>
+
+</template>
+
+<script lang="ts">
+import {
+     IonPage,
+     IonContent,
+     IonHeader,
+     IonToolbar,
+     IonTitle,
+     IonButtons,
+     IonButton,
+     modalController
+} from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+     name: 'simple-modal',
+     components: {
+          IonPage,
+          IonContent,
+          IonHeader,
+          IonToolbar,
+          IonTitle,
+          IonButtons,
+          IonButton
+     },
+     props: {
+          title: {
+               type: String,
+               default: 'Simple Modal'
+          }
+     },
+     data() {
+          return {
+               content: 'Content'
+          }
+     },
+     methods: {
+          dismissModal: () => {
+               modalController.dismiss();
+          }
+     }
+});
+</script>
+
+<style scoped>
+
+</style>
