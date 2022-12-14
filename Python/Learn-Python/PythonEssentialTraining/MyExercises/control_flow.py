85d277c7ea51d19d9e4cf15489769eb49e89f099
Ahmed Alkatheeri <3767316+aakatheeri@users.noreply.github.com>
2022-12-14 12:36:33 +0400

    Add Python examples.

---
+++ Python/Learn-Python/PythonEssentialTraining/MyExercises/control_flow.py
@@ -0,0 +1,29 @@
+### Blocks of code in Python separated by     tab
+
+## If / Else statement
+
+a = True
+b = False
+c = True
+if a:
+    print('It is true!')
+    print('Also print this')
+    if b:
+        print('both are true')
+        if c:
+            print('All are true')
+else:
+    print('It is false');
+print('Always print this')
+
+## For loops
+
+a = [1,2,3,4,5]
+# for number in a:
+#     print(number)
+
+## While loops
+a = 0
+while a <= 5:
+    print(a)
+    a = a + 1
---
+++ Python/Learn-Python/PythonEssentialTraining/MyExercises/functions.py
@@ -0,0 +1,19 @@
+## Functions
+
+def multiplyByThree(val):
+    return 3 * val
+
+def multiply(val1, val2):
+    return val1 * val2
+
+def appendFour(myList):
+    myList.append(4)
+
+print (multiplyByThree(2))
+print (multiply(2,3))
+
+a = [1,2,3]
+appendFour(a)
+print(a)
+
+print( print('Hello World!') )
